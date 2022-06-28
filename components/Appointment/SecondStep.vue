f93860330e74614ed35252301c1e495204e6de51
Pavel Kramarenko <pavelk@sweetsoft.com.ua>
2022-06-28 18:37:48 +0300

    add calendar

--- components/Appointment/SecondStep.vue
+++ components/Appointment/SecondStep.vue
@@ -1,34 +1,77 @@
 <template>
   <div class="d-flex flex-column justify-content-center align-items-center">
-    <p>{{ currentDate.toLocaleString("en-us", { weekday: "long" }) }}</p>
-    <h1>{{ currentDate.getDate() }}</h1>
+    <p>{{ minDate.toLocaleString("en-us", { weekday: "long" }) }}</p>
+    <h1>{{ minDate.getDate() }}</h1>
     <h2>
-      {{ months[currentDate.getMonth()] }} {{ currentDate.getFullYear() }}
+      {{ months[minDate.getMonth()] }} {{ minDate.getFullYear() }}
     </h2>
-   
+    <DatePicker
+      :min-date="minDate"
+      :max-date="maxDate"
+      :attributes="attributes"
+      v-model="date"
+      title-position="left"
+      :first-day-of-week="2"
+      class="col mt-3 mx-2"
+    />
   </div>
 </template>
 
 <script>
 export default {
   name: "AppointmentSecondStep",
-  data: () => ({
-    currentDate: new Date(),
-    months: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-  }),
+  data: () => {
+    const minDate = new Date();
+    const maxDate = new Date(
+      minDate.getFullYear(),
+      minDate.getMonth() + 2,
+      minDate.getDate()
+    );
+    const dates = [];
+    let date = minDate;
+    do {
+      const d = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate() + 1
+      );
+      dates.push(d);
+      date = d;
+    } while (date < maxDate);
+    return {
+      date: null,
+      minDate,
+      maxDate,
+      attributes: [
+        {
+          key: "today",
+          dot: true,
+          dates: new Date(),
+        },
+        {
+          highlight: {
+            color: "blue",
+            fillMode: "light",
+          },
+          dates: [minDate, ...dates],
+        },
+      ],
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+    };
+  },
 };
 </script>
 
