b3e4755865ca6444f5f0db3b698784d708dadb82
Pavel Kramarenko <pavelk@sweetsoft.com.ua>
2022-06-28 18:09:58 +0300

    update appointment second step

--- components/Appointment/SecondStep.vue
+++ components/Appointment/SecondStep.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
-    <p>sdfghjkl</p>
+  <div class="d-flex flex-column justify-content-center align-items-center">
+    <p>{{ currentDate.toLocaleString("en-us", { weekday: "long" }) }}</p>
+    <h1>{{ currentDate.getDate() }}</h1>
+    <h2>
+      {{ months[currentDate.getMonth()] }} {{ currentDate.getFullYear() }}
+    </h2>
+   
   </div>
 </template>
 
 <script>
 export default {
   name: "AppointmentSecondStep",
+  data: () => ({
+    currentDate: new Date(),
+    months: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+  }),
 };
 </script>
 
-<style>
+<style scoped>
 </style>
