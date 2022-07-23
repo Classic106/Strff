386c567f9ee7ea1742ee941cbc624c1c404c6b5c
Pavel Kramarenko <pavelk@sweetsoft.com.ua>
2022-07-24 00:32:05 +0300

    add custom scroll to cart products

--- components/Cart/FirstStep.vue
+++ components/Cart/FirstStep.vue
@@ -10,216 +10,225 @@
       <h6 class="text-uppercase text-center mb-3">
         total price: {{ totalPrice | formatNumber }} $
       </h6>
-      <ul v-if="order_items.length" class="p-0">
-        <li v-for="item in order_items" :key="item.product.id" class="p-3 mb-3">
-          <div class="d-flex flex-column">
-            <div class="d-flex">
-              <div class="w-25">
-                <img
-                  :src="`${getFirstImage(item.product.image)}`"
-                  class="m-auto gold-border"
-                />
-              </div>
-              <div class="d-flex flex-column px-3">
-                <h6>{{ item.product.title }}</h6>
-                <div>
-                  <p class="mb-2 grey">
-                    price {{ item.product.price | formatNumber }} $
+      <vueCustomScrollbar
+        class="scroll-area position-realtive px-3"
+        :settings="settings"
+      >
+        <ul v-if="order_items.length" class="p-0">
+          <li
+            v-for="item in order_items"
+            :key="item.product.id"
+            class="p-3 mb-3"
+          >
+            <div class="d-flex flex-column">
+              <div class="d-flex">
+                <div class="w-25">
+                  <img
+                    :src="`${getFirstImage(item.product.image)}`"
+                    class="m-auto gold-border"
+                  />
+                </div>
+                <div class="d-flex flex-column px-3">
+                  <h6>{{ item.product.title }}</h6>
+                  <div>
+                    <p class="mb-2 grey">
+                      price {{ item.product.price | formatNumber }} $
+                    </p>
+                  </div>
+                  <p v-if="item.quantity > 1">
+                    total price
+                    {{ (item.product.price * item.quantity) | formatNumber }} $
                   </p>
                 </div>
-                <p v-if="item.quantity > 1">
-                  total price
-                  {{ (item.product.price * item.quantity) | formatNumber }} $
-                </p>
               </div>
-            </div>
-            <div class="d-flex flex-column mt-3">
-              <div class="d-flex">
-                <p class="w-25 mb-2 grey">description</p>
-                <p class="mb-2">{{ item.product.description }}</p>
-              </div>
-              <div class="d-flex">
-                <p class="w-25 mb-2 grey">quantity</p>
+              <div class="d-flex flex-column mt-3">
                 <div class="d-flex">
-                  <button
-                    v-if="edit"
-                    v-on:click="quantityMinus(item.id)"
-                    class="gold-background"
+                  <p class="w-25 mb-2 grey">description</p>
+                  <p class="mb-2">{{ item.product.description }}</p>
+                </div>
+                <div class="d-flex">
+                  <p class="w-25 mb-2 grey">quantity</p>
+                  <div class="d-flex">
+                    <button
+                      v-if="edit"
+                      v-on:click="quantityMinus(item.id)"
+                      class="gold-background"
+                    >
+                      -
+                    </button>
+                    <p class="my-auto" :class="edit && 'mx-3'">
+                      {{ item.quantity }}
+                    </p>
+                    <button
+                      v-if="edit"
+                      v-on:click="quantityPlus(item.id)"
+                      class="gold-background"
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div class="d-flex">
+                  <p class="w-25 mb-2 grey">category</p>
+                  <p
+                    v-for="category in item.product.categories"
+                    :key="category.id"
+                    class="mr-2 mb-2"
                   >
-                    -
-                  </button>
-                  <p class="my-auto" :class="edit && 'mx-3'">
-                    {{ item.quantity }}
+                    {{ category.name }}
                   </p>
-                  <button
-                    v-if="edit"
-                    v-on:click="quantityPlus(item.id)"
-                    class="gold-background"
-                  >
-                    +
-                  </button>
+                </div>
+                <div class="d-flex">
+                  <p class="w-25 mb-2 grey">Purchase</p>
+                  <p class="mb-2" v-if="!edit">
+                    {{
+                      purchaseTypes.filter(
+                        (type) => item.purchase_type.id === type.id
+                      )[0].title
+                    }}
+                  </p>
+                  <PurchaseTypes
+                    v-else
+                    cart
+                    :purType="item.purchase_type ? item.purchase_type.id : null"
+                    :subType="
+                      item.subscription_type ? item.subscription_type.id : null
+                    "
+                    v-on:setTypes="(types) => setTypes(types, item.id)"
+                  />
                 </div>
               </div>
-              <div class="d-flex">
-                <p class="w-25 mb-2 grey">category</p>
-                <p
-                  v-for="category in item.product.categories"
-                  :key="category.id"
-                  class="mr-2 mb-2"
-                >
-                  {{ category.name }}
-                </p>
-              </div>
-              <div class="d-flex">
-                <p class="w-25 mb-2 grey">Purchase</p>
-                <p class="mb-2" v-if="!edit">
-                  {{
-                    purchaseTypes.filter(
-                      (type) => item.purchase_type.id === type.id
-                    )[0].title
-                  }}
-                </p>
-                <PurchaseTypes
-                  v-else
-                  cart
-                  :purType="item.purchase_type ? item.purchase_type.id : null"
-                  :subType="
-                    item.subscription_type ? item.subscription_type.id : null
-                  "
-                  v-on:setTypes="(types) => setTypes(types, item.id)"
-                />
-              </div>
-            </div>
-            <div class="w-100 d-flex justify-content-between">
-              <div
-                class="d-flex align-items-center pen"
-                v-on:click="edit = !edit"
-              >
-                <span class="icon icon-pen m-2"></span>
-                <p class="m-0 pl-1 gold">{{ edit ? "save" : "edit" }}</p>
-              </div>
-              <span
-                class="icon icon-trash m-2"
-                v-on:click="removeProduct(item.id)"
-              ></span>
-            </div>
-          </div>
-        </li>
-      </ul>
-      <ul v-if="order_bundles.length" class="p-0">
-        <li
-          v-for="(order_bundle, index) in order_bundles"
-          :key="order_bundle.id"
-          class="p-3 mb-3"
-        >
-          <div class="d-flex flex-column">
-            <div class="d-flex row">
-              <div v-if="(index - 1) % 2" class="col-4">
+              <div class="w-100 d-flex justify-content-between">
                 <div
-                  v-if="order_bundle.bundle.products[index + 1]"
-                  class="row p-2"
+                  class="d-flex align-items-center pen"
+                  v-on:click="edit = !edit"
                 >
-                  <div class="col-5 p-0">
-                    <div class="m-auto p-2">
-                      <img
-                        :src="`${getFirstImage(
-                          order_bundle.bundle.products[index].image
-                        )}`"
-                        class="m-auto gold-border"
-                      />
+                  <span class="icon icon-pen m-2"></span>
+                  <p class="m-0 pl-1 gold">{{ edit ? "save" : "edit" }}</p>
+                </div>
+                <span
+                  class="icon icon-trash m-2"
+                  v-on:click="removeProduct(item.id)"
+                ></span>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <ul v-if="order_bundles.length" class="p-0">
+          <li
+            v-for="(order_bundle, index) in order_bundles"
+            :key="order_bundle.id"
+            class="p-3 mb-3"
+          >
+            <div class="d-flex flex-column">
+              <div class="d-flex row">
+                <div v-if="(index - 1) % 2" class="col-4">
+                  <div
+                    v-if="order_bundle.bundle.products[index + 1]"
+                    class="row p-2"
+                  >
+                    <div class="col-5 p-0">
+                      <div class="m-auto p-2">
+                        <img
+                          :src="`${getFirstImage(
+                            order_bundle.bundle.products[index].image
+                          )}`"
+                          class="m-auto gold-border"
+                        />
+                      </div>
+                      <div
+                        class="d-flex flex-column justify-content-between mt-3"
+                      >
+                        <span class="font-weight-light text-center">
+                          {{ order_bundle.bundle.products[index].title }}
+                        </span>
+                        <span class="font-weight-light text-center grey">
+                          ${{
+                            order_bundle.bundle.products[index].price
+                              | formatNumber
+                          }}
+                        </span>
+                      </div>
                     </div>
-                    <div
-                      class="d-flex flex-column justify-content-between mt-3"
-                    >
-                      <span class="font-weight-light text-center">
-                        {{ order_bundle.bundle.products[index].title }}
-                      </span>
-                      <span class="font-weight-light text-center grey">
-                        ${{
-                          order_bundle.bundle.products[index].price
-                            | formatNumber
-                        }}
-                      </span>
+                    <div class="col-2 p-0 mx-auto mt-3">
+                      <p class="text-center plus grey">+</p>
+                    </div>
+                    <div class="col-5 p-0">
+                      <div class="m-auto p-2">
+                        <img
+                          :src="`${getFirstImage(
+                            order_bundle.bundle.products[index + 1].image
+                          )}`"
+                          class="m-auto gold-border"
+                        />
+                      </div>
+                      <div
+                        class="d-flex flex-column justify-content-between mt-3"
+                      >
+                        <span class="font-weight-light text-center">
+                          {{ order_bundle.bundle.products[index + 1].title }}
+                        </span>
+                        <span class="font-weight-light text-center grey">
+                          ${{
+                            order_bundle.bundle.products[index + 1].price
+                              | formatNumber
+                          }}
+                        </span>
+                      </div>
                     </div>
                   </div>
-                  <div class="col-2 p-0 mx-auto mt-3">
-                    <p class="text-center plus grey">+</p>
-                  </div>
-                  <div class="col-5 p-0">
-                    <div class="m-auto p-2">
-                      <img
-                        :src="`${getFirstImage(
-                          order_bundle.bundle.products[index + 1].image
-                        )}`"
-                        class="m-auto gold-border"
-                      />
-                    </div>
-                    <div
-                      class="d-flex flex-column justify-content-between mt-3"
-                    >
-                      <span class="font-weight-light text-center">
-                        {{ order_bundle.bundle.products[index + 1].title }}
-                      </span>
-                      <span class="font-weight-light text-center grey">
-                        ${{
-                          order_bundle.bundle.products[index + 1].price
-                            | formatNumber
-                        }}
-                      </span>
+                  <div v-else class="d-flex justify-content-center">
+                    <div class="col-5 p-0">
+                      <div class="m-auto p-2">
+                        <img
+                          :src="`${getFirstImage(
+                            order_bundle.bundle.products[index].image
+                          )}`"
+                          class="m-auto gold-border"
+                        />
+                      </div>
+                      <div
+                        class="d-flex flex-column justify-content-between mt-3"
+                      >
+                        <span class="font-weight-light text-center">
+                          {{ order_bundle.bundle.products[index].title }}
+                        </span>
+                        <span class="font-weight-light text-center grey">
+                          ${{
+                            order_bundle.bundle.products[index].price
+                              | formatNumber
+                          }}
+                        </span>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div v-else class="d-flex justify-content-center">
-                  <div class="col-5 p-0">
-                    <div class="m-auto p-2">
-                      <img
-                        :src="`${getFirstImage(
-                          order_bundle.bundle.products[index].image
-                        )}`"
-                        class="m-auto gold-border"
-                      />
-                    </div>
-                    <div
-                      class="d-flex flex-column justify-content-between mt-3"
-                    >
-                      <span class="font-weight-light text-center">
-                        {{ order_bundle.bundle.products[index].title }}
-                      </span>
-                      <span class="font-weight-light text-center grey">
-                        ${{
-                          order_bundle.bundle.products[index].price
-                            | formatNumber
-                        }}
-                      </span>
-                    </div>
-                  </div>
+                <div class="mt-4 d-flex flex-column col-7">
+                  <p class="text-uppercase font-weight-bold m-0">
+                    bundle price: ${{ order_bundle.bundle.price }}
+                  </p>
+                  <p class="save-price">
+                    You save: $
+                    {{
+                      order_bundle.bundle.price -
+                      order_bundle.bundle.products.reduce(
+                        (prVal, curVal) => prVal + curVal.price,
+                        0
+                      )
+                    }}!
+                  </p>
                 </div>
               </div>
-              <div class="mt-4 d-flex flex-column col-7">
-                <p class="text-uppercase font-weight-bold m-0">
-                  bundle price: ${{ order_bundle.bundle.price }}
-                </p>
-                <p class="save-price">
-                  You save: $
-                  {{
-                    order_bundle.bundle.price -
-                    order_bundle.bundle.products.reduce(
-                      (prVal, curVal) => prVal + curVal.price,
-                      0
-                    )
-                  }}!
-                </p>
+              <div class="w-100 d-flex justify-content-end">
+                <span
+                  class="icon icon-trash m-2"
+                  v-on:click="removeBundle(order_bundle.id)"
+                ></span>
               </div>
             </div>
-            <div class="w-100 d-flex justify-content-end">
-              <span
-                class="icon icon-trash m-2"
-                v-on:click="removeBundle(order_bundle.id)"
-              ></span>
-            </div>
-          </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </vueCustomScrollbar>
       <button
         class="text-uppercase p-3 mt-auto gold-background"
         v-on:click="$emit('isShipping')"
@@ -238,7 +247,14 @@ import PurchaseTypes from "~/components/common/PurchaseTypes";
 export default {
   props: ["isOpen", "isShipping"],
   components: { PurchaseTypes },
-  data: () => ({ edit: false, totalPrice: 0 }),
+  data: () => ({
+    edit: false,
+    totalPrice: 0,
+    settings: {
+      suppressScrollX: true,
+      wheelPropagation: false,
+    },
+  }),
   computed: {
     ...mapGetters({
       order_items: "order/getOrderItems",
@@ -316,6 +332,10 @@ export default {
   background: #333333;
   z-index: 1;
   height: 90vh;
+}
+
+.scroll-area {
+  height: 65vh;
 }
 
 .grey {
