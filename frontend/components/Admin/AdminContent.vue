ab5995d602acb6abfbee21007223eab8d9fc2960
Classic106 <40822873+Classic106@users.noreply.github.com>
2022-09-16 16:41:11 +0300

    add admin page

---
+++ frontend/components/Admin/AdminContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ page }}</div>
+</template>
+
+<script>
+export default {
+  name: "AdminContent",
+  props: {
+    page: String,
+    default: "home",
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/components/Admin/AdminHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex justify-content-between align-items-center py-3">
+    <div class="d-flex align-items-center">
+      <span class="px-3">strf</span>
+      <span class="px-2">{{ getDate() }}</span>
+    </div>
+    <div>
+      <input v-model="text" type="text" placeholder="Search" />
+    </div>
+    <div class="px-3">user</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdminHeader",
+  data: () => ({
+    text: "",
+  }),
+  methods: {
+    getDate: function () {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = date.getMonth();
+      const day = date.getDay();
+
+      return `${day}/${month}/${year}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
