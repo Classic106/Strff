1e189897c2587aa3d9aef1190bcab897769af8d1
Classic106 <40822873+Classic106@users.noreply.github.com>
2022-12-15 22:40:58 +0200

    add tests & add admin component test

---
+++ frontend/tests/unit/admin.test.js
@@ -0,0 +1,59 @@
+import { mount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuex from "vuex";
+
+import Admin from "@/components/Admin";
+import Loader from "@/components/common/Loader.vue";
+
+Vue.use(Vuex);
+
+describe("Admin", () => {
+  let getters;
+  let store;
+
+  const mockRoute = { path: "/orders" };
+  const mockRouter = { push: jest.fn() };
+
+  beforeEach(() => {
+    getters = {
+      "auth/user": () => ({
+        role: {
+          type: "",
+        },
+      }),
+    };
+
+    store = new Vuex.Store({ getters });
+  });
+
+  test("Admin returns Loader", () => {
+    const wrapperAdmin = mount(Admin, {
+      store,
+      mocks: {
+        $route: mockRoute,
+        $router: mockRouter,
+      },
+    });
+    const wrapperLoader = mount(Loader);
+    expect(wrapperAdmin.html()).toContain(wrapperLoader.html());
+  });
+
+  test("Admin without authenticated user", () => {
+    expect(mockRouter.push).toHaveBeenCalledWith("/admin/login");
+  });
+
+  test("Admin with authenticated user", () => {
+    getters = {
+      "auth/user": () => ({
+        role: {
+          type: "authenticated",
+        },
+      }),
+    };
+
+    store = new Vuex.Store({
+      getters,
+    });
+    expect(mockRouter.push).toHaveBeenCalledWith("/admin/login");
+  });
+});
