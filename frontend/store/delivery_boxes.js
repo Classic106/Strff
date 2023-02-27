f29d4fece15607f19e7d560b94af0d0ee24d551f
Classic106 <40822873+Classic106@users.noreply.github.com>
2023-02-27 22:24:30 +0200

    replace box & items examples data

--- frontend/store/delivery_boxes.js
+++ frontend/store/delivery_boxes.js
@@ -1,93 +1,9 @@
 import qs from "qs";
 import { error } from "~/utils/error";
+import { delivery_boxes } from "~/static/delivery_boxes_examples";
 
 export const state = () => ({
-  boxes: [
-    {
-      w: 3,
-      l: 2,
-      h: 2,
-      v: 12,
-      weight: 21937,
-    },
-    {
-      w: 6,
-      l: 6,
-      h: 6,
-      v: 216,
-      weight: 21937,
-    },
-    {
-      w: 6,
-      l: 6,
-      h: 8,
-      v: 288,
-      weight: 21937,
-    },
-    {
-      w: 8,
-      l: 8,
-      h: 8,
-      v: 512,
-      weight: 21937,
-    },
-    {
-      w: 6,
-      l: 3,
-      h: 2,
-      v: 36,
-      weight: 1513,
-    },
-    {
-      w: 1,
-      l: 3,
-      h: 3,
-      v: 9,
-      weight: 11561,
-    },
-    {
-      w: 19.375,
-      l: 17.375,
-      h: 14,
-      v: 4712.96875,
-      weight: 17475,
-    },
-    {
-      w: 17.5,
-      l: 12.5,
-      h: 3,
-      v: 656.25,
-      weight: 22074,
-    },
-    {
-      w: 38,
-      l: 7.5,
-      h: 6.5,
-      v: 1852.5,
-      weight: 24277,
-    },
-    {
-      w: 13,
-      l: 11,
-      h: 2,
-      v: 286,
-      weight: 14514,
-    },
-    {
-      w: 16,
-      l: 11,
-      h: 3,
-      v: 528,
-      weight: 1734,
-    },
-    {
-      w: 18,
-      l: 16,
-      h: 3,
-      v: 864,
-      weight: 11002,
-    },
-  ],
+  boxes: delivery_boxes,
 });
 
 export const actions = {
